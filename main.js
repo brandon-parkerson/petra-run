097182790392288fc666fc7bf6aec92c39f07635
Brandon Parkerson <bpkp1996@Osmo.>
2025-06-18 13:54:21 +0300

    player jump

--- main.js
+++ main.js
@@ -1,17 +1,52 @@
+
+
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
+function main() {
+    const player = {
+        x: 50,
+        y: 150,
+        width: 25,
+        height: 25,
+        color: "white",
+        velocityY: 0,
+        isJumping: false,
+        gravity: 0.5,
+        jumpStrength: -10,
+        draw: function() {
+            ctx.fillStyle = this.color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        },
+        update: function() {
+            this.velocityY += this.gravity;
+            this.y += this.velocityY;
 
-const player = {
-    x: 50,
-    y: 50,
-    height: 10,
-    width: 10,
-    color: "blue",
-    draw: function() {
-        ctx.fillStyle = this.color;
-        ctx.fillRect(25, 25, 50, 50);
+            // Ground collision
+            if (this.y + this.height >= canvas.height) {
+                this.y = canvas.height - this.height;
+                this.velocityY = 0;
+                this.isJumping = false;
+            }
+        }
+    };
+
+    document.addEventListener("keydown", function(e) {
+        if (e.code === "Space" && !player.isJumping) {
+            player.velocityY = player.jumpStrength;
+            player.isJumping = true;
+        }
+    });
+
+    function gameLoop() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        player.update();
+        player.draw();
+        requestAnimationFrame(gameLoop);
     }
+
+    gameLoop();
 }
 
-player.draw();
+main();
+
